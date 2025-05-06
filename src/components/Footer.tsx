f4388a4eab5e84455c63a63afac42458aa4e87b9
Mauriciofgdo198 <119525503+Mauriciofgdo198@users.noreply.github.com>
2025-05-06 14:48:42 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="bg-[#151c33] text-white">
+    <footer className="relative bg-gradient-to-r from-colegio-azul to-colegio-azulClaro text-white overflow-hidden">
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Unidade Contagem */}
