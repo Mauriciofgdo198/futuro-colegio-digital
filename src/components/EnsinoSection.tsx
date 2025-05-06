2b8a12a33e190fbe7be93aff0dae5eba784f5b43
Mauriciofgdo198 <119525503+Mauriciofgdo198@users.noreply.github.com>
2025-05-06 14:58:37 -0300

    Update EnsinoSection.tsx

--- src/components/EnsinoSection.tsx
+++ src/components/EnsinoSection.tsx
@@ -17,7 +17,7 @@ const EnsinoSection = () => {
       descricao: "Consolidamos a alfabetização e o raciocínio lógico, promovendo autonomia e consciência social.",
       icone: <GraduationCap className="h-10 w-10 text-colegio-azul" />,
       cor: "border-t-colegio-azul",
-      imagem: "https://drive.google.com/file/d/1Qto67Eo1kmyNYqFFDE9HxS29m8kPsQVi/view?usp=sharing"
+      imagem: "https://images.unsplash.com/photo-1509869175650-a1d97972541a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       titulo: "Ensino Fundamental II",
